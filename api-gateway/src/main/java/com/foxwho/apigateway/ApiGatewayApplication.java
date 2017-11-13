ef92fc56b50013f84ed848e05f0d9de2ca5b0ca8
fox <foxiswho@gmail.com>
2017-11-13 11:16:02 +0800

    请求过滤

--- api-gateway/src/main/java/com/foxwho/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/foxwho/apigateway/ApiGatewayApplication.java
@@ -1,8 +1,10 @@
 package com.foxwho.apigateway;
 
+import com.foxwho.apigateway.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -10,5 +12,9 @@ public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+	@Bean
+	public AccessFilter accessFilter(){
+		return new AccessFilter();
 	}
 }
